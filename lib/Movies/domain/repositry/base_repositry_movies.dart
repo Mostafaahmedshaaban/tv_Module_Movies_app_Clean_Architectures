373580da443cecfb11350a763e82bb6d5ad0269d
Your Name <you@example.com>
2024-01-28 21:37:32 +0200

    create finish domain layer

--- lib/Movies/domain/repositry/base_repositry_movies.dart
+++ lib/Movies/domain/repositry/base_repositry_movies.dart
@@ -1,18 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_tv_app/Movies/core/failure/failure.dart';
 import 'package:movie_tv_app/Movies/domain/entities/details.dart';
 import 'package:movie_tv_app/Movies/domain/entities/movies.dart';
 import 'package:movie_tv_app/Movies/domain/entities/recommendations.dart';
+import 'package:movie_tv_app/Movies/domain/usecase/get_details_movies_usecase.dart';
+import 'package:movie_tv_app/Movies/domain/usecase/get_recommendations_movies_usecase.dart';
 
 abstract class BaseRepositryMovies
 {
-  Future<List<Movies>>getnowplayingmovies();
+  Future<Either<Failure,List<Movies>>>getnowplayingmovies();
 
-  Future<List<Movies>>getpopularmovies();
+  Future<Either<Failure,List<Movies>>>getpopularmovies();
 
-  Future<List<Movies>>gettopratedmovies();
+  Future<Either<Failure,List<Movies>>>gettopratedmovies();
 
-  Future<List<Recommendations>>getrecommendationsmovies();
+  Future<Either<Failure,List<Recommendations>>>getrecommendationsmovies(RecommendationsParameters parameters);
 
-  Future<Details>getdetailsmovies();
-
-
+  Future<Either<Failure,Details>>getdetailsmovies(DetailsParameters parameters);
 }
