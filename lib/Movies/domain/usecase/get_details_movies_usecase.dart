dbc1efa4c75e0cb0339c82550fa0b4252c967851
Your Name <you@example.com>
2024-01-28 21:36:31 +0200

    create  usecase details

---
+++ lib/Movies/domain/usecase/get_details_movies_usecase.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:movie_tv_app/Movies/core/failure/failure.dart';
+import 'package:movie_tv_app/Movies/core/usecase/base_usecase.dart';
+import 'package:movie_tv_app/Movies/domain/entities/details.dart';
+import 'package:movie_tv_app/Movies/domain/repositry/base_repositry_movies.dart';
+
+class GetDetailsMoviesUsecase extends BaseUseCase<Details,DetailsParameters>  {
+  final BaseRepositryMovies baseRepositryMovies;
+  GetDetailsMoviesUsecase(this.baseRepositryMovies);
+
+  @override
+  Future<Either<Failure, Details>> call(DetailsParameters parameters) async{
+    return await baseRepositryMovies.getdetailsmovies(parameters);
+  }
+
+}
+
+class DetailsParameters extends Equatable {
+  final int DetailId;
+
+  DetailsParameters(this.DetailId);
+
+  @override
+  List<Object> get props => [DetailId];
+}
