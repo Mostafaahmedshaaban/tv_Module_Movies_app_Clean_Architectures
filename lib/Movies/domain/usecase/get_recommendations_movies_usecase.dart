7bfa925f7e6a970b13c82ced3c219d63243ecf62
Your Name <you@example.com>
2024-01-28 21:36:08 +0200

    create  usecase recommendations

---
+++ lib/Movies/domain/usecase/get_recommendations_movies_usecase.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:movie_tv_app/Movies/core/failure/failure.dart';
+import 'package:movie_tv_app/Movies/core/usecase/base_usecase.dart';
+import 'package:movie_tv_app/Movies/domain/entities/recommendations.dart';
+import 'package:movie_tv_app/Movies/domain/repositry/base_repositry_movies.dart';
+class GetRecommendationsMoviesUsecase extends BaseUseCase<List<Recommendations>,RecommendationsParameters>{
+ final BaseRepositryMovies baseRepositryMovies;
+ GetRecommendationsMoviesUsecase(this.baseRepositryMovies);
+  @override
+  Future<Either<Failure, List<Recommendations>>> call(RecommendationsParameters parameters)async {
+    return await  baseRepositryMovies.getrecommendationsmovies(parameters);
+  }
+}
+class RecommendationsParameters extends Equatable {
+  final String id;
+  RecommendationsParameters(this.id);
+  @override
+  List<Object?> get props => [id];
+}
