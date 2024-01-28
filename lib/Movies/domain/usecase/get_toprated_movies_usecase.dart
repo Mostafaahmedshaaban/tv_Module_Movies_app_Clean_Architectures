297cdcb0e41b1e4804662e87428132c9a375b2b1
Your Name <you@example.com>
2024-01-28 21:36:46 +0200

    create  usecase toprated

---
+++ lib/Movies/domain/usecase/get_toprated_movies_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_tv_app/Movies/core/failure/failure.dart';
+import 'package:movie_tv_app/Movies/core/usecase/base_usecase.dart';
+import 'package:movie_tv_app/Movies/domain/entities/movies.dart';
+import 'package:movie_tv_app/Movies/domain/repositry/base_repositry_movies.dart';
+
+class GetTopRatedMoviesusecase extends BaseUseCase<List<Movies>,NoParameters>{
+final  BaseRepositryMovies baseRepositryMovies;
+ GetTopRatedMoviesusecase(this.baseRepositryMovies);
+  @override
+  Future<Either<Failure, List<Movies>>> call(NoParameters parameters) async
+  {
+    return await  baseRepositryMovies.gettopratedmovies();
+  }
+
+}
